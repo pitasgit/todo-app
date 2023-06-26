cb66661996cd77f5a5d64a334bfd024cfdd76233
pitasgit <silviupetreu@gmail.com>
2023-06-26 20:08:35 +0300

    Implement the Add button

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
+FILEPATH = "todos.txt"
 
-FILEPATH = "todos_item.txt"
 
 def get_todos(filepath=FILEPATH):
     """Read a text file and return the list of to-do items."""
@@ -12,7 +12,9 @@ def write_todos(todos_arg, filepath="todos.txt"):
     """Write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
+
+
 print(__name__)
 if __name__ == "__main__":
-    print ("hello from functions")
+    print("hello from functions")
     print(get_todos())
--- gui.py
+++ gui.py
@@ -6,9 +6,25 @@ import PySimpleGUI as sg
 # window.close()
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo") # am pus key=# "todo"
+                                                            # pentru a fi cheia din dictionar pentru ceea ce am adaugat
 add_button = sg.Button("Add")
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window('My To-Do App',
+                   layout=[[label], [input_box], [add_button]],
+                   font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo= values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
