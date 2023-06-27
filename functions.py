0b529f5e9d800bbb5bc18c918d72ec4f7697de2d
pitasgit <silviupetreu@gmail.com>
2023-06-27 19:56:50 +0300

    Implement the Edit button

--- functions.py
+++ functions.py
@@ -8,7 +8,7 @@ def get_todos(filepath=FILEPATH):
     return todos_local
 
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """Write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
